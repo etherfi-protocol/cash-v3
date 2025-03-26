cc9dd892c46e1d3570d694f155bd6a690f2d1a17
Shivam Agrawal <shivam@ether.fi>
2025-03-26 16:12:35 -0500

    Upgrade open ocean module on dev

--- scripts/DeployOpenOceanModule.s.sol
+++ scripts/DeployOpenOceanModule.s.sol
@@ -5,6 +5,7 @@ import {stdJson} from "forge-std/StdJson.sol";
 
 import { OpenOceanSwapModule } from "../src/modules/openocean-swap/OpenOceanSwapModule.sol";
 import {Utils, ChainConfig} from "./utils/Utils.sol";
+import {EtherFiDataProvider} from "../src/data-provider/EtherFiDataProvider.sol";
 
 contract DeployOpenOceanModule is Utils {
     address openOceanSwapRouter = 0x6352a56caadC4F1E25CD6c75970Fa768A3304e64;
