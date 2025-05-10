06280ea7ec092017ae9dddb99ecd108bb74fe485
Shivam Agrawal <shivam@ether.fi>
2025-05-10 12:37:42 +0530

    added script for upgrade for withdraw bug fix

--- scripts/gnosis-txs/UpgradeWithdrawalFix.s.sol
+++ scripts/gnosis-txs/UpgradeWithdrawalFix.s.sol
@@ -7,6 +7,7 @@ import { Test } from "forge-std/Test.sol";
 
 import { CashModuleCore } from "../../src/modules/cash/CashModuleCore.sol";
 import { CashModuleSetters } from "../../src/modules/cash/CashModuleSetters.sol";
+import { CashEventEmitter } from "../../src/modules/cash/CashEventEmitter.sol";
 import { DebtManagerCore } from "../../src/debt-manager/DebtManagerCore.sol";
 import { GnosisHelpers } from "../utils/GnosisHelpers.sol";
 import { Utils } from "../utils/Utils.sol";
@@ -15,11 +16,28 @@ contract UpgradeWithdrawalFix is GnosisHelpers, Utils, Test {
     address cashControllerSafe = 0xA6cf33124cb342D1c604cAC87986B965F428AAC4;
 
     address cashModule;
+    address cashEventEmitter;
     address debtManager;
+    address dataProvider;
 
     address cashModuleCoreImpl;
     address cashModuleSettersImpl;
+    address cashEventEmitterImpl;
     address debtManagerCoreImpl;
+
+    address[] withdrawTokens;
+    bool[] shouldWhitelist;
+
+    address public weth = 0x5300000000000000000000000000000000000004;
+    address public weEth = 0x01f0a31698C4d065659b9bdC21B3610292a1c506;
+    address public usdc = 0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4;
+    address public liquidEth = 0xf0bb20865277aBd641a307eCe5Ee04E79073416C;   
+    address public liquidUsd = 0x08c6F91e2B681FaF5e17227F2a44C307b3C1364C;
+    address public liquidBtc = 0x5f46d540b6eD704C3c8789105F30E075AA900726;
+    address public eUsd = 0x939778D83b46B456224A33Fb59630B11DEC56663;
+    address public eBtc = 0x657e8C867D8B37dCC18fA4Caead9C45EB088C642;
+    address public scr = 0xd29687c813D741E2F938F4aC377128810E217b1b;
+    address public ethfi = 0x056A5FA5da84ceb7f93d36e545C5905607D8bD81;
 
     function run() public {
         string memory deployments = readDeploymentFile();
@@ -36,8 +54,11 @@ contract UpgradeWithdrawalFix is GnosisHelpers, Utils, Test {
             deployments,
             string.concat(".", "addresses", ".", "DebtManager")
         );
-
-        address dataProvider = stdJson.readAddress(
+        cashEventEmitter = stdJson.readAddress(
+            deployments,
+            string.concat(".", "addresses", ".", "CashEventEmitter")
+        );
+        dataProvider = stdJson.readAddress(
             deployments,
             string.concat(".", "addresses", ".", "EtherFiDataProvider")
         );
@@ -45,6 +66,9 @@ contract UpgradeWithdrawalFix is GnosisHelpers, Utils, Test {
         cashModuleCoreImpl = address(new CashModuleCore(dataProvider));
         cashModuleSettersImpl = address(new CashModuleSetters(dataProvider));
         debtManagerCoreImpl = address(new DebtManagerCore(dataProvider));
+        cashEventEmitterImpl = address(new CashEventEmitter(cashModule));
+
+        _buildWithdrawArrays();
 
         string memory txs = _getGnosisHeader(chainId, addressToHex(cashControllerSafe));
 
@@ -55,13 +79,55 @@ contract UpgradeWithdrawalFix is GnosisHelpers, Utils, Test {
         txs = string(abi.encodePacked(txs, _getGnosisTransaction(addressToHex(cashModule), cashModuleCoreUpgrade, "0", false)));
 
         string memory cashModuleSettersUpgrade = iToHex(abi.encodeWithSelector(CashModuleCore.setCashModuleSettersAddress.selector, cashModuleSettersImpl));
-        txs = string(abi.encodePacked(txs, _getGnosisTransaction(addressToHex(cashModule), cashModuleSettersUpgrade, "0", true)));
+        txs = string(abi.encodePacked(txs, _getGnosisTransaction(addressToHex(cashModule), cashModuleSettersUpgrade, "0", false)));
+
+        string memory cashEventEmitterUpgrade = iToHex(abi.encodeWithSelector(UUPSUpgradeable.upgradeToAndCall.selector, cashEventEmitterImpl, ""));
+        txs = string(abi.encodePacked(txs, _getGnosisTransaction(addressToHex(cashEventEmitter), cashEventEmitterUpgrade, "0", false)));
         
+        string memory whitelistWithdrawAssets = iToHex(abi.encodeWithSelector(CashModuleSetters.configureWithdrawAssets.selector, withdrawTokens, shouldWhitelist));
+        txs = string(abi.encodePacked(txs, _getGnosisTransaction(addressToHex(cashModule), whitelistWithdrawAssets, "0", true)));
+
         vm.createDir("./output", true);
         string memory path = "./output/UpgradeWithdrawalFix.json";
         vm.writeFile(path, txs);
 
         vm.stopBroadcast();
+
         executeGnosisTransactionBundle(path);
+
+        assertTrue(CashModuleCore(cashModule).isWhitelistedWithdrawAsset(weth));
+        assertTrue(CashModuleCore(cashModule).isWhitelistedWithdrawAsset(weEth));
+        assertTrue(CashModuleCore(cashModule).isWhitelistedWithdrawAsset(usdc));
+        assertTrue(CashModuleCore(cashModule).isWhitelistedWithdrawAsset(liquidEth));
+        assertTrue(CashModuleCore(cashModule).isWhitelistedWithdrawAsset(liquidUsd));
+        assertTrue(CashModuleCore(cashModule).isWhitelistedWithdrawAsset(liquidBtc));
+        assertTrue(CashModuleCore(cashModule).isWhitelistedWithdrawAsset(eUsd));
+        assertTrue(CashModuleCore(cashModule).isWhitelistedWithdrawAsset(eBtc));
+        assertTrue(CashModuleCore(cashModule).isWhitelistedWithdrawAsset(scr));
+        assertTrue(CashModuleCore(cashModule).isWhitelistedWithdrawAsset(ethfi));
+    }
+
+    function _buildWithdrawArrays() internal {
+        withdrawTokens.push(weth);
+        withdrawTokens.push(weEth);
+        withdrawTokens.push(usdc);
+        withdrawTokens.push(liquidEth);
+        withdrawTokens.push(liquidUsd);
+        withdrawTokens.push(liquidBtc);
+        withdrawTokens.push(eUsd);
+        withdrawTokens.push(eBtc);
+        withdrawTokens.push(scr);
+        withdrawTokens.push(ethfi);
+
+        shouldWhitelist.push(true);
+        shouldWhitelist.push(true);
+        shouldWhitelist.push(true);
+        shouldWhitelist.push(true);
+        shouldWhitelist.push(true);
+        shouldWhitelist.push(true);
+        shouldWhitelist.push(true);
+        shouldWhitelist.push(true);
+        shouldWhitelist.push(true);
+        shouldWhitelist.push(true);
     }
 }
