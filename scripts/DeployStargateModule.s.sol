2273345f706f68a6f99a2324054a0d2c54b60b75
Shivam Agrawal <shivam@ether.fi>
2025-06-17 12:25:40 -0500

    deploy stargate module

---
+++ scripts/DeployStargateModule.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import { stdJson } from "forge-std/StdJson.sol";
+
+import { Utils, ChainConfig } from "./utils/Utils.sol";
+import { StargateModule } from "../src/modules/stargate/StargateModule.sol";
+
+contract DeployStargateModule is Utils {
+    StargateModule stargateModule;
+
+    address weETH = 0x01f0a31698C4d065659b9bdC21B3610292a1c506;
+    address usdc = 0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4;
+
+    address usdcStargatePool = 0x3Fc69CC4A842838bCDC9499178740226062b14E4;
+
+    function run() public {
+        vm.startBroadcast();
+
+        string memory deployments = readDeploymentFile();
+        address dataProvider = stdJson.readAddress(
+            deployments, 
+            string(abi.encodePacked(".", "addresses", ".", "EtherFiDataProvider"))
+        );
+
+        address[] memory assets = new address[](2);
+        assets[0] = address(usdc);
+        assets[1] = address(weETH);
+
+        StargateModule.AssetConfig[] memory assetConfigs = new StargateModule.AssetConfig[](2);
+
+        assetConfigs[0] = StargateModule.AssetConfig({
+            isOFT: false,
+            pool: usdcStargatePool
+        });
+        assetConfigs[1] = StargateModule.AssetConfig({
+            isOFT: true,
+            pool: address(weETH)
+        });
+
+        stargateModule = new StargateModule(assets, assetConfigs, dataProvider);
+
+        vm.stopBroadcast();
+    }
+}
