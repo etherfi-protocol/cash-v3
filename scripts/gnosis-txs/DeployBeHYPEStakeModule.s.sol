2140ee52422ff1d74e8d0d8aaf2a6226344aa8a6
jtfirek <firekjt@gmail.com>
2025-11-11 16:06:36 -0500

    revert testing

---
+++ scripts/gnosis-txs/DeployBeHYPEStakeModule.s.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import { stdJson } from "forge-std/StdJson.sol";
+
+import { BeHYPEStakeModule } from "../../src/modules/hype/BeHYPEStakeModule.sol";
+import { EtherFiDataProvider } from "../../src/data-provider/EtherFiDataProvider.sol";
+import { GnosisHelpers } from "../utils/GnosisHelpers.sol";
+import { Utils } from "../utils/Utils.sol";
+
+contract DeployBeHYPEStakeModule is GnosisHelpers, Utils {
+    address cashControllerSafe = 0xA6cf33124cb342D1c604cAC87986B965F428AAC4;
+
+    address l2BeHypeStaker = 0x3f1Bdae959cEd680E434Fe201861E97976eA4A8F;
+    address whypeToken = 0xd83E3d560bA6F05094d9D8B3EB8aaEA571D1864E;
+    address beHypeToken = 0xA519AfBc91986c0e7501d7e34968FEE51CD901aC;
+
+    BeHYPEStakeModule public beHypeStakeModule;
+
+    function run() public {
+        string memory deployments = readDeploymentFile();
+        string memory chainId = vm.toString(block.chainid);
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        address dataProvider = stdJson.readAddress(
+            deployments,
+            string.concat(".", "addresses", ".", "EtherFiDataProvider")
+        );
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        address moduleAddress = deployWithCreate3(
+            abi.encodePacked(
+                type(BeHYPEStakeModule).creationCode,
+                abi.encode(
+                    dataProvider,
+                    l2BeHypeStaker,
+                    whypeToken,
+                    beHypeToken,
+                    refundGasLimit
+                )
+            ),
+            getSalt(BEHYPE_STAKE_MODULE)
+        );
+
+
+        vm.stopBroadcast();
+    }
+}
+
