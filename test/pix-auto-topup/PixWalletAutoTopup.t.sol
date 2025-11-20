0fcd05d08369fd0ec4e49df7b5950ecbb8b98a7f
Shivam Agrawal <shivam@ether.fi>
2025-11-20 10:01:47 -0500

    fix audit findings

--- test/pix-auto-topup/PixWalletAutoTopup.t.sol
+++ test/pix-auto-topup/PixWalletAutoTopup.t.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.28;
 
 import { Test } from "forge-std/Test.sol";
+import { IERC1967 } from "@openzeppelin/contracts/interfaces/IERC1967.sol";
 
-import { PixWalletAutoTopup, Ownable, IERC20 } from "../../src/pix-auto-topup/PixWalletAutoTopup.sol";
+import { PixWalletAutoTopup, Ownable, IERC20, UUPSUpgradeable } from "../../src/pix-auto-topup/PixWalletAutoTopup.sol";
 import { UUPSProxy } from "../../src/UUPSProxy.sol";
 
 contract PixWalletAutoTopupTest is Test {
@@ -28,6 +29,8 @@ contract PixWalletAutoTopupTest is Test {
     function test_setPixWalletOnBase() public {
         address newPixWalletOnBase = makeAddr("newPixWalletOnBase");
         vm.prank(owner);
+        vm.expectEmit(true, true, true, true);
+        emit PixWalletAutoTopup.PixWalletSet(pixWalletOnBase, newPixWalletOnBase);
         pixWalletAutoTopup.setPixWalletOnBase(newPixWalletOnBase);
         assertEq(pixWalletAutoTopup.pixWalletOnBase(), newPixWalletOnBase);
 
@@ -51,5 +54,30 @@ contract PixWalletAutoTopupTest is Test {
 
         uint256 balance = IERC20(pixWalletAutoTopup.USDC()).balanceOf(address(pixWalletAutoTopup));
         assertEq(balance, 0);
+    }
+
+    function test_bridgeViaCCTP_whenAmountIsGreaterThanBalance() public {
+        uint256 amount = 10e6;
+        uint256 balance = amount / 2;
+        deal(pixWalletAutoTopup.USDC(), address(pixWalletAutoTopup), balance);
+
+        vm.expectEmit(true, true, true, true);
+        emit PixWalletAutoTopup.BridgeViaCCTP(pixWalletAutoTopup.USDC(), balance, pixWalletAutoTopup.CCTP_DEST_DOMAIN_BASE(), bytes32(uint256(uint160(pixWalletOnBase))));
+        pixWalletAutoTopup.bridgeViaCCTP(amount);
+
+        uint256 balanceAfter = IERC20(pixWalletAutoTopup.USDC()).balanceOf(address(pixWalletAutoTopup));
+        assertEq(balanceAfter, 0);
+    }
+
+    function test_onlyOwnerCanUpgradeTheContract() public {
+        address newImplementation = address(new PixWalletAutoTopup());
+        vm.prank(makeAddr("notOwner"));
+        vm.expectRevert(Ownable.Unauthorized.selector);
+        UUPSUpgradeable(address(pixWalletAutoTopup)).upgradeToAndCall(newImplementation, "");
+
+        vm.prank(owner);
+        vm.expectEmit(true, true, true, true);
+        emit IERC1967.Upgraded(newImplementation);
+        UUPSUpgradeable(address(pixWalletAutoTopup)).upgradeToAndCall(newImplementation, "");
     }
 }
