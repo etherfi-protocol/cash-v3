3db7f524c48e33379576dd321f01a477232467aa
jtfirek <jacob@ether.fi>
2025-12-03 16:52:23 -0500

    refactor tests

--- test/safe/modules/cash/settlement-dispatcher/SettlementDispatcher.t.sol
+++ test/safe/modules/cash/settlement-dispatcher/SettlementDispatcher.t.sol
@@ -15,11 +15,8 @@ import { IEtherFiDataProvider } from "../../../../../src/interfaces/IEtherFiData
 import { IBoringOnChainQueue } from "../../../../../src/interfaces/IBoringOnChainQueue.sol";
 import { IDebtManager } from "../../../../../src/interfaces/IDebtManager.sol";
 import { MessagingFee } from "../../../../../src/interfaces/IStargate.sol";
-import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { UpgradeableProxy } from "../../../../../src/utils/UpgradeableProxy.sol";
 import { SettlementDispatcher } from "../../../../../src/settlement-dispatcher/SettlementDispatcher.sol";
-import { SettlementDispatcherV2 } from "../../../../../src/settlement-dispatcher/SettlementDispatcherV2.sol";
-import { BinSponsor } from "../../../../../src/interfaces/ICashModule.sol";
 import { Constants } from "../../../../../src/utils/Constants.sol";
 
 contract SettlementDispatcherTest is CashModuleTestSetup, Constants {
@@ -559,54 +556,5 @@ contract SettlementDispatcherTest is CashModuleTestSetup, Constants {
             useCanonicalBridge: false,
             minGasLimit: 0
         });
-    }
-
-    // ============ V2 Tests ============
-
-    function updateToV2() internal {
-        address settlementDispatcherV2Impl = address(new SettlementDispatcherV2(BinSponsor.Reap, address(dataProvider)));
-        vm.prank(owner);
-        UUPSUpgradeable(address(settlementDispatcherReap)).upgradeToAndCall(settlementDispatcherV2Impl, "");
-    }
-
-    function test_v2_setRefundWallet_succeeds() public {
-        updateToV2();
-        
-        SettlementDispatcherV2 v2 = SettlementDispatcherV2(payable(address(settlementDispatcherReap)));
-        address newWallet = makeAddr("newWallet");
-        
-        vm.prank(owner);
-        vm.expectEmit(true, true, true, true);
-        emit SettlementDispatcherV2.RefundWalletSet(newWallet);
-        v2.setRefundWallet(newWallet);
-        
-        assertEq(v2.getRefundWallet(), newWallet);
-    }
-
-    function test_v2_getRefundWallet_fallsBackToDataProvider() public {
-        updateToV2();
-        
-        SettlementDispatcherV2 v2 = SettlementDispatcherV2(payable(address(settlementDispatcherReap)));
-        
-        assertEq(v2.getRefundWallet(), refundWallet);
-        
-        address customWallet = makeAddr("customWallet");
-        vm.prank(owner);
-        v2.setRefundWallet(customWallet);
-        assertEq(v2.getRefundWallet(), customWallet);
-        
-        vm.prank(owner);
-        v2.setRefundWallet(address(0));
-        assertEq(v2.getRefundWallet(), refundWallet);
-    }
-
-    function test_v2_setRefundWallet_reverts_whenNotOwner() public {
-        updateToV2();
-        
-        SettlementDispatcherV2 v2 = SettlementDispatcherV2(payable(address(settlementDispatcherReap)));
-        
-        vm.prank(alice);
-        vm.expectRevert(UpgradeableProxy.OnlyRoleRegistryOwner.selector);
-        v2.setRefundWallet(makeAddr("newWallet"));
     }
 }
