3a4347b0a152d0656e3dedb589350b318b237be4
jtfirek <jacob@ether.fi>
2026-02-17 11:21:19 -0500

    audit fixes

--- src/interfaces/IFraxCustodian.sol
+++ src/interfaces/IFraxCustodian.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.28;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 interface IFraxCustodian {
-    function deposit(uint256 amountIn, address reciever) external payable returns (uint256 shares);
-    function redeem(uint256 sharesIn, address reciever, address owner) external returns (uint256 amountOut);
+    function deposit(uint256 amountIn, address receiver) external payable returns (uint256 shares);
+    function redeem(uint256 sharesIn, address receiver, address owner) external returns (uint256 amountOut);
 }
